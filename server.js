482fad4910bb80c090a8dbb9f4a9247a2f770309
Vivekmasona <112853103+Vivekmasona@users.noreply.github.com>
2025-09-02 23:12:04 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,33 +1,30 @@
-// server.js
-import express from "express";
-import http from "http";
-import { Server } from "socket.io";
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
 
 const app = express();
 const server = http.createServer(app);
-const io = new Server(server, {
-  cors: { origin: "*" }
-});
+const io = socketIo(server);
 
-const onlineSessions = new Set();
+let onlineSessions = new Set();
 
-io.on("connection", (socket) => {
-  console.log("User connected:", socket.id);
+io.on('connection', (socket) => {
+  console.log('User connected:', socket.id);
 
-  socket.on("joinSession", (sessionId) => {
+  socket.on('joinSession', (sessionId) => {
     socket.sessionId = sessionId;
     onlineSessions.add(sessionId);
-
-    // Notify other users with same sessionId
-    socket.broadcast.emit("sessionOnline", { sessionId });
+    socket.broadcast.emit('sessionOnline', { sessionId });
   });
 
-  socket.on("disconnect", () => {
+  socket.on('disconnect', () => {
     if (socket.sessionId) {
       onlineSessions.delete(socket.sessionId);
-      socket.broadcast.emit("sessionOffline", { sessionId: socket.sessionId });
+      socket.broadcast.emit('sessionOffline', { sessionId: socket.sessionId });
     }
   });
 });
 
-server.listen(3000, () => console.log("Server running on port 3000"));
+server.listen(3000, () => {
+  console.log('Server running on port 3000');
+});
