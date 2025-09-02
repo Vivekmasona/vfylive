a104efb0945e732b84927ea1a11b1025d6274c02
Vivekmasona <112853103+Vivekmasona@users.noreply.github.com>
2025-09-03 00:29:29 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,32 +1,26 @@
-import express from "express";
-import http from "http";
-import { Server } from "socket.io";
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
 
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server, { cors: { origin: "*" } });
 
-const sessions = {}; // { sessionId: Set of socketIds }
+const sessions = {}; // sessionId -> set of socketIds
 
-io.on('connection', (socket) => {
+io.on("connection", (socket) => {
     let currentSession = null;
 
-    socket.on('join-session', (sessionId) => {
+    socket.on("join-session", (sessionId) => {
         currentSession = sessionId;
         if (!sessions[sessionId]) sessions[sessionId] = new Set();
         sessions[sessionId].add(socket.id);
 
-        // Emit online count
-        io.to(sessionId).emit('update-online', sessions[sessionId].size);
+        io.to(sessionId).emit("update-online", sessions[sessionId].size);
         socket.join(sessionId);
     });
 
-    socket.on('disconnect', () => {
+    socket.on("disconnect", () => {
         if (currentSession && sessions[currentSession]) {
             sessions[currentSession].delete(socket.id);
-            io.to(currentSession).emit('update-online', sessions[currentSession].size);
-        }
-    });
-});
-
-server.listen(3000, () => console.log('Server running on 3000'));
+            io.to(currentSession).emit("
